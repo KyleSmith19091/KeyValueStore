d37d961e42199a4789f02e04ca1cc2ee731692ac
Kyle Smith <sKorpion19091@gmail.com>
2021-07-16 15:42:17 +0200

    ⚡️ Added KeyValue files

---
+++ include/KeyValue.hpp
@@ -0,0 +1,21 @@
+#ifndef KEYVALUE_HPP
+#define KEYVALUE_HPP 
+
+#include "./Stack.hpp"
+#include "./Transaction.hpp"
+
+#include <unordered_map>
+#include <string>
+
+class KeyValue {
+	private:
+		Stack<Transaction>* transactions = new Stack<Transaction>();
+		std::unordered_map<std::string,std::string> globalStore;
+			
+	public:
+		KeyValue();
+		~KeyValue();
+		void addTransaction();
+};
+
+#endif
