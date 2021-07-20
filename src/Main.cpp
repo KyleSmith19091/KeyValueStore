52ff9c494e6b8822dfdb5eeff3d88a587f2e9a92
Kyle Smith <sKorpion19091@gmail.com>
2021-07-20 13:24:58 +0200

    ⚡️ Added shell to entry point

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,14 +2,15 @@
 #include "../include/Stack.hpp"
 #include "../include/Node.hpp"
 #include "../include/Transaction.hpp"
+#include "../include/Shell.hpp"
 
 using namespace std;
 
 int main()
 {
-	// Transaction stack test
-	Stack<Transaction>* t = new Stack<Transaction>();
-	t->push(new Transaction("k","v"));
+	// Interface into key-value store
+	Shell s(">");
+	s.init();
 
 	return 0;
 }
