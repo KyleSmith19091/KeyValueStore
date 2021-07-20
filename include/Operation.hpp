cdef71dbc5a75782b24871c3851a130d13ce03ba
Kyle Smith <sKorpion19091@gmail.com>
2021-07-20 13:24:09 +0200

    ⚡️ Operation and Statement abstraction added

---
+++ include/Operation.hpp
@@ -0,0 +1,9 @@
+#ifndef OPERATION_HPP
+#define OPERATION_HPP
+
+enum Operation {
+	SET,
+	GET
+};
+
+#endif
---
+++ src/Statement.cpp
@@ -0,0 +1,19 @@
+#include "../include/Statement.hpp"
+
+Statement::Statement(Operation op, int v) {
+	this->operation = op;
+	this->value = v;
+}
+
+std::string Statement::getString() noexcept {
+	std::string operationString = "";
+	if(this->operation == Operation::GET) {
+		operationString = "GET";		
+	} else if(this->operation == Operation::SET) {
+		operationString = "SET";
+	} else {
+		operationString = "UNKNOWN";
+	}
+
+	return operationString + " " + std::to_string(this->value);
+}
