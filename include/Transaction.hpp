24c48582c2dbf76ffae06f088cb86f4885737887
Kyle Smith <sKorpion19091@gmail.com>
2021-07-25 17:35:58 +0200

    ⚡️ Added CRUD operations for transactions

--- include/Transaction.hpp
+++ include/Transaction.hpp
@@ -2,6 +2,8 @@
 #define TRANSACTION_HPP
 #define citer std::unordered_map<std::string,int>::const_iterator
 
+#include "Statement.hpp"
+
 #include <unordered_map>
 #include <string>
 
@@ -11,19 +13,6 @@ class Transaction{
 		// Local store => Will be written to global store on commit
 		std::unordered_map<std::string,int> store;
 
-	public:
-		// Start transaction with initial operation
-		Transaction() noexcept; 
-
-		// Start transaction with context of previous transaction
-		Transaction(const Transaction& tx);
-
-		// Wrapper around convertMapToString() for more 'semantic' code
-		std::string getTransactionString() const noexcept;
-
-		// Local store getter
-		std::unordered_map<std::string,int> getLocalStore() const noexcept;
-
 		// Set given key to value, can also be used to update
 		void set (const std::string& key, int value);
 
@@ -32,6 +21,25 @@ class Transaction{
 
 		// Delete given key
 		void deleteKey (const std::string& key); 
+
+	public:
+		// Start transaction with initial operation
+		Transaction() noexcept; 
+
+		// Start transaction with context of previous transaction
+		Transaction(const Transaction&);
+
+		// Wrapper around convertMapToString() for more 'semantic' code
+		std::string getTransactionString() const noexcept;
+
+		// Local store getter
+		std::unordered_map<std::string,int> getLocalStore() const noexcept;
+
+		// GET statement to read data from store
+		int readLocalStore(const Statement&) const;
+
+		void writeLocalStore(const Statement&);
+
 
 };
 #endif
--- src/Transaction.cpp
+++ src/Transaction.cpp
@@ -1,6 +1,6 @@
 #include "../include/Transaction.hpp"
 
-// Start transaction with initial operation
+// Create initial transaction
 Transaction::Transaction() noexcept {}
 
 // Start transaction with context of previous transaction
@@ -8,17 +8,35 @@ Transaction::Transaction(const Transaction& tx) {
 	this->store = tx.getLocalStore();
 }
 
-// Wrapper around convertMapToString() for more 'semantic' code
+// Transaction map to string conversion
 std::string Transaction::getTransactionString() const noexcept {
 	std::string output = "";
 	for(citer i = this->store.cbegin(); i != this->store.cend(); ++i){
-		output += "[" + i->first + "] => " + std::to_string(i->second);	
+		output += "[" + i->first + "] => " + std::to_string(i->second) + "\n";	
 	}
 	return output;
 }
 
-// Local store functions //
+// Local store getter
+std::unordered_map<std::string,int> Transaction::getLocalStore() const noexcept {
+	return this->store;
+}
 
+// Read Statement
+int Transaction::readLocalStore(const Statement& s) const {
+	return get(s.key); 
+}
+
+// Write Statement
+void Transaction::writeLocalStore(const Statement& s) {
+	if(s.operation == Operation::SET) {
+		set(s.key, s.value);
+	} else if(s.operation == Operation::DELETE) {
+		deleteKey(s.key);
+	}
+}
+
+// Local store functions //
 // Set given key to value, can also be used to update
 void Transaction::set (const std::string& key, int value) {
 	this->store[key] = value;
